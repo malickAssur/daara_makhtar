05469a6ff62f333db6922f379f22393a6aa19751
malick <elmn64@yahoo.fr>
2014-11-20 21:30:09 +0100

    mail

--- application/controllers/NousContacterController.php
+++ application/controllers/NousContacterController.php
@@ -18,7 +18,21 @@ class NousContacterController extends Zend_Controller_Action
         if ($this->getRequest()->isPost()) {
 	        $formData = $this->getRequest()->getPost();
 	        if ($form->isValid($formData)) {
-	           
+	           $prenom =  $formData['prenom'];
+	           $nom =  $formData['nom'];
+	           $email =  $formData['email'];
+	           $phone =  $formData['phone'];
+	           $siteWeb =  $formData['siteWeb'];
+	           $message =  $formData['message'];
+	           	$fullMessage= "".$message;
+				$mail = new Zend_Mail();
+				$mail->setBodyText($fullMessage );
+				$mail->setBodyHtml($fullMessage );
+				$mail->setFrom('elmn64@yahoo.fr', 'Un expéditeur');
+				$mail->addTo('elmn64@yahoo.fr', 'Un destinataire');
+				$mail->setSubject('message depuis Daara Mawloud');
+				$mail->send();
+
 	            $this->_helper->redirector('index');
 	        } else {
 	            
