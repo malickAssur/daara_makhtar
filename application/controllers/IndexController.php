354947e846ec3930dba48c87a803945a72f9a9d4
malick <ehmndiaye@consulting.altensir.fr>
2014-11-20 18:29:51 +0100

    ajout articles page nous contacter

--- application/controllers/IndexController.php
+++ application/controllers/IndexController.php
@@ -6,6 +6,8 @@ class IndexController extends Zend_Controller_Action
     public function init()
     {
         /* Initialize action controller here */
+      /* $this->view->headLink()->appendStylesheet( $this->view->baseUrl('css/carroussel.css'));
+       $this->view->headScript()->prependFile( $this->view->baseUrl('/js/carroussel.js'));*/
     }
 
     public function indexAction()
---
+++ application/forms/NousContacter.php
@@ -0,0 +1,15 @@
+<?php
+
+class Application_Form_NousContacter extends Zend_Form
+{
+
+    public function init()
+    {
+        /* Form Elements & Other Definitions Here ... */
+    }
+    
+    
+
+
+}
+
